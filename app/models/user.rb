1dca17770ff106d46ccde7aa9bdd7365bb3b9150
高橋亜友美 <kooszs12@gmail.com>
2023-08-05 12:06:28 +0000

    [Add] validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #バリデーション設定
+  validates :nickname,
+    #文字数制限
+    length: { minimum: 1, maximum: 10 },
+    #ニックネームの一意性
+    uniqueness: true
 end
