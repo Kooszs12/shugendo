c6452aa9f20010c12e1cc9c25c1a2dc1ee61238b
高橋亜友美 <kooszs12@gmail.com>
2023-08-06 11:50:25 +0000

    [Add] place/viewは一旦完成

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -5,6 +5,19 @@ class Admin::PlacesController < ApplicationController
   end
 
   def create
+    #空の変数作成
+    byebug
+    @place = Place.new(place_params)
+    #保存が成功したら
+    #byebug
+    if @place.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to place_path(@place.id)
+    #失敗したら
+    else
+      @places = Place.all
+      render :top
+    end
   end
 
   def index
@@ -19,7 +32,7 @@ class Admin::PlacesController < ApplicationController
    private
 
   def place_params
-    params.require(:place).permit(:prefecture_id, :user_id, :admin_id, :type, :name, :address, :postcode, :phone_number, :god, :sect, :goshuin_status, :pet_status)
+    params.require(:place).permit(:prefecture_id, :user_id, :admin_id, :type, :name, :address, :postcode, :phone_number, :got, :sect, :goshuin_status, :pet_status)
   end
 
 end
