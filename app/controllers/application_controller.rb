f9c69d30b1cd780aaefcc1845fff0e186933a167
高橋亜友美 <kooszs12@gmail.com>
2023-08-10 11:59:41 +0000

    [Update] devise日本語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+#deveisで許可されていないからnicknameを許可してあげないといけない
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+end
+
 end
