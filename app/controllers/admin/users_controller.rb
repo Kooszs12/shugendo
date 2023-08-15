d1f006e81b8f9c6e490e1136248189f5c5b01ea9
高橋亜友美 <kooszs12@gmail.com>
2023-08-15 00:24:22 +0000

    [Start] 8/15

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,25 @@
 class Admin::UsersController < ApplicationController
+
+  # アクセス制限
+  before_action :authenticate_admin!
+
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @goshuins = @user.goshuins.page(params[:page]).per(10) # ページネーションを適用（１ページ１０件表示）
+    @goshuin_names = @goshuins.map { |goshuin| goshuin.place.name }
+    @goshuin_users = @goshuins.map { |goshuin| goshuin.user.nickname }
+    @goshuin_prefectures = @goshuins.map { |goshuin| goshuin.place.prefecture }
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get "/" => "homes#top"
     resources :places, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-    resources :users, only: [:index, :edit, :update, :destroy]
+    resources :users, only: [:index, :show, :edit, :update, :destroy]
   end
 
   scope module: :user do
